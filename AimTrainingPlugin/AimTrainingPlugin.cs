4c04ad07783457a9c8eeec6b1394392474c0b4b2
Infinium1 <jeremychernin@gmail.com>
2021-01-16 22:40:50 -0500

    Basic set up

---
+++ AimTrainingPlugin/AimTrainingPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins {
+	[Info("Aim Training Plugin", "Fyre", "0.0.1")]
+	public class AimTrainingPlugin : RustPlugin {
+		void Init() {
+			
+		}
+	}
+}
